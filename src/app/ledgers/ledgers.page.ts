4ddfd8210f497f709e5a1084ea732e98f0f5b768
Spicer Matthews <github@cloudmanic.com>
2019-04-14 17:41:04 -0700

    Cleaned up a few things on the ledger

--- src/app/ledgers/ledgers.page.ts
+++ src/app/ledgers/ledgers.page.ts
@@ -1,4 +1,12 @@
+//
+// Date: 2019-04-14
+// Author: Spicer Matthews (spicer@skyclerk.com)
+// Last Modified by: Spicer Matthews
+// Copyright: 2019 Cloudmanic Labs, LLC. All rights reserved.
+//
+
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
 	selector: 'app-ledgers',
@@ -6,12 +14,80 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LedgersPage implements OnInit {
-
 	tabs: string = "ledgers";
 
-	constructor() { }
+	//
+	// Constructor.
+	//
+	constructor(private alertController: AlertController) { }
 
 	ngOnInit() {
 	}
 
+	//
+	// doAccounts - Show the accounts selector.
+	//
+	async doAccounts() {
+		const alert = await this.alertController.create({
+			header: 'Radio',
+			inputs: [
+				{
+					name: 'radio1',
+					type: 'radio',
+					label: 'Radio 1',
+					value: 'value1',
+					checked: true
+				},
+				{
+					name: 'radio2',
+					type: 'radio',
+					label: 'Radio 2',
+					value: 'value2'
+				},
+				{
+					name: 'radio3',
+					type: 'radio',
+					label: 'Radio 3',
+					value: 'value3'
+				},
+				{
+					name: 'radio4',
+					type: 'radio',
+					label: 'Radio 4',
+					value: 'value4'
+				},
+				{
+					name: 'radio5',
+					type: 'radio',
+					label: 'Radio 5',
+					value: 'value5'
+				},
+				{
+					name: 'radio6',
+					type: 'radio',
+					label: 'Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 ',
+					value: 'value6'
+				}
+			],
+			buttons: [
+				{
+					text: 'Cancel',
+					role: 'cancel',
+					cssClass: 'secondary',
+					handler: () => {
+						console.log('Confirm Cancel');
+					}
+				}, {
+					text: 'Ok',
+					handler: () => {
+						console.log('Confirm Ok');
+					}
+				}
+			]
+		});
+
+		await alert.present();
+	}
 }
+
+/* End File */
