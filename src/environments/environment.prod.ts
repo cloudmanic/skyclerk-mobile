6e44c5c4dcf6af1e7e97b33ded1ac87895258683
Spicer Matthews <github@cloudmanic.com>
2019-04-14 22:16:39 -0700

    more clean up for deployment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
 	client_id: "XL8TeRGBdsUvvM3",
+	//app_server: "https://app.skyclerk.com",
 	app_server: "http://192.168.1.100:9090",
-	site_url: "http://localhost:8100"
 };
