73c9fa6c6dfad64e00b01cd83ebeb50ff0f13782
Spicer Matthews <github@cloudmanic.com>
2019-11-11 15:54:16 -0800

    Updated the client_id in production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	version: "3.0.0",
-	client_id: "tggbdwRHPNPg&H/6QZ9eQtAxo",
+	client_id: "XL8TeRGBdsUvvM3",
 	app_server: "https://app.skyclerk.com",
 	mixpanel_key: "5974bde1c90a5606add2695a17b2db10"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
 	production: false,
 	version: "3.0.0",
-	client_id: "XL8TeRGBdsUvvM3",
+	client_id: "abc123wootsmobile",
 	app_server: "http://127.0.0.1:9090",
 	//app_server: "http://192.168.1.100:9090"
 
-	//mixpanel_key: "",
-	mixpanel_key: "5974bde1c90a5606add2695a17b2db10",
+	mixpanel_key: "",
+	//mixpanel_key: "5974bde1c90a5606add2695a17b2db10",
 };
 
 /*
